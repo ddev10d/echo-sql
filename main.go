b7dce2c43c75b0e39d529ac41b4a911678b35a3e
divakar bhardwaj <dbhardwaj@contecglobal.com>
2023-08-18 15:47:59 +0530

    initial commit

---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"database/sql"
+	"os"
+
+	"github.com/keploy/go-sdk/integrations/kecho/v4"
+
+	"github.com/keploy/go-sdk/keploy"
+	"github.com/labstack/echo/v4"
+	_ "github.com/lib/pq"
+	"go.uber.org/zap"
+)
+
+var port = "8082"
+
+var Logger *zap.Logger
+
+var Database *sql.DB
+
+func main() {
+	if os.Getenv("PORT") != "" {
+		port = os.Getenv("PORT")
+	}
+	var err error
+	Logger, _ = zap.NewProduction()
+	defer Logger.Sync() // flushes buffer
+
+	Database, err = NewConnection(ConnectionDetails{
+		host:     "localhost",
+		port:     "5438",
+		user:     "postgres",
+		password: "postgres",
+		db_name:  "postgres",
+	})
+
+	if err != nil {
+		Logger.Fatal("Failed to establish connection to local PostgreSQL instance:", zap.Error(err))
+	}
+
+	defer Database.Close()
+
+	// init Keploy
+	k := keploy.New(keploy.Config{
+		App: keploy.AppConfig{
+			Name: "sample-url-shortener",
+			Port: port,
+		},
+	})
+
+	r := echo.New() // Init echo
+
+	// kecho.EchoV4(k, r) // Tie echo router in with Keploy
+	r.Use(kecho.EchoMiddlewareV4(k))
+
+	r.GET("/:param", GetURL)
+	r.POST("/url", PutURL)
+	r.GET("/stream", StreamPutURL)
+	r.DELETE("/:param", DeleteURL)
+	r.PUT("/:param", UpdateURL)
+	err = r.Start(":" + port)
+	if err != nil {
+		panic(err)
+	}
+
+}
